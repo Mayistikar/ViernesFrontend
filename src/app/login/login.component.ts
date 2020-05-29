a386982659b679bce54f8331a7a4f88c27bf3f14
Mayistikar <andersonrodriguezce@gmail.com>
2020-05-29 05:43:36 -0500

    wip

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public user: any = {};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, 
+              private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.user.name = "Anderson";
+    this.user.pass = "1990";
   }
 
+  onSumbit(){
+    let actionUrl;
+    const data = {...this.user};
+  //   if(this.userId){
+  //     delete data['id'];
+  //     delete data['user'];
+  //     delete data['pass'];
+  //     actionUrl = `${this.url}user/${this.userId}/update`;
+  //   }else{
+  //     actionUrl = `${this.url}user/create`;
+  //   }
+  
+  //   this.http.post(actionUrl, data).subscribe((response: any) => {
+  //     this.saveState = true;
+  //   }, (error) => this.saveState = false);   
+  }
 }
